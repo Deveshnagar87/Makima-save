85001f79e1f5d36f607a464a603210f63031c077
Deveshnagar87 <nagardevesh2004@gmail.com>
2025-08-04 08:14:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8249003232:AAH4k2G18-GY96w41DcpsQ88IfivGwVSho8"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7364404381").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://nagardeveshjpr:vDBSHxlkeAfVAWs8@cluster0.slvhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002752207790")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002626193023"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002592814806"))
